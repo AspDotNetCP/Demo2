31f73b366cbf32863a0e7f2a9a973075b9bc1b62
Asp <key.man@gmx.com>
2017-01-08 22:57:07 +0800

    Add class RegisteViewModel

--- src/Demo2/Models/Identity/RegisterViewModel.cs
+++ src/Demo2/Models/Identity/RegisterViewModel.cs
@@ -6,15 +6,19 @@ using System.Threading.Tasks;
 
 namespace Demo2.Models.Identity
 {
-    public class LoginViewModel
+    public class RegisterViewModel
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
-
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Comfirm Password")]
+        [Compare("Password", ErrorMessage = "Password Invalid")]
+        public string ComfirmPassword { get; set; }
     }
 }
