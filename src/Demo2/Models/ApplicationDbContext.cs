f9dfabe3896ba1d00d47f99863db0ae2904032f9
Asp <key.man@gmx.com>
2017-01-08 20:27:02 +0800

    ApplicationDbContext:IdentityDbContext<ApplicationUser>

---
+++ src/Demo2/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo2.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Demo2.Models
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+    }
+}
