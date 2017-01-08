ccde1b78b50e38f78d5e8231f69d92559d0012ab
Asp <key.man@gmx.com>
2017-01-08 22:51:50 +0800

    RegisterViewModel

--- src/Demo2/Models/Identity/LoginViewModel.cs
+++ src/Demo2/Models/Identity/LoginViewModel.cs
@@ -9,8 +9,29 @@ namespace Demo2.Models.Identity
     public class LoginViewModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
-        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Comfirm Password")]
+        [Compare("Password",ErrorMessage = "Password Invalid")]
+        public string ComfirmPassword { get; set; }
 
     }
 }
